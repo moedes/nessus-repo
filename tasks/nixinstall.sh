e934470a76755da421236d0f468f6c7cf776be5d
Jerry Mozes <jerry@puppet.com>
2020-05-01 16:38:58 -0600

    Revert "Add logic to scripts for actions"

--- tasks/nixinstall.sh
+++ tasks/nixinstall.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-echo $PT_action
+rpm -ivh "$PT_nixfilepath"
+/opt/nessus_agent/sbin/nessuscli agent link --host="$PT_host" --key="$PT_key" --port="$PT_port"
+/bin/systemctl start nessusagent.service 
 
-install () {
-    
-    rpm -ivh "$PT_nixfilepath"
-    /opt/nessus_agent/sbin/nessuscli agent link --host="$PT_host" --key="$PT_key" --port="$PT_port" --groups="Linux Servers - Mozes"
-    /bin/systemctl start nessusagent.service 
-
-    # rm -rf "$PT_nixfilepath"
-}
-
-unlink () {
-
-    /opt/nessus_agent/sbin/nessuscli agent unlink
-
-}
-
-if [ "$PT_action" = install ]
-then
-    install
-elif [ "$PT_action" = unlink ]
-then    
-    unlink
-else   
-    echo "Please use a valid action.  Valid actions are install or unlink"
-fi
-
+rm -rf "$PT_nixfilepath"
